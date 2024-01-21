9ccf152b18230f557993afa4d312036049a38823
Estey <30854444+VoidWind-ST@users.noreply.github.com>
2024-01-20 19:39:26 -0500

    Update setup_system.sh

--- setup_system.sh
+++ setup_system.sh
@@ -35,6 +35,27 @@ sudo snap install htop
 sudo apt install build-essential
 
 # --------------------------------------------------
+# neofetch module
+sudo apt install neofetch
+
+# --------------------------------------------------
+# powertop module
+sudo apt install powertop
+
+# --------------------------------------------------
+# python3 
+sudo apt install python3
+
+# --------------------------------------------------
+# anaconda 
+# 	Prerequisites pack:
+sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+# 	Replace <INSTALLER_VERSION> with the version of the installer file you want to download
+# All installers can be found at repo.anaconda.com/archive
+#curl -O https://repo.anaconda.com/archive/Anaconda3-<INSTALLER_VERSION>-Linux-x86_64.sh
+
+
+# --------------------------------------------------
 # Nvidia utilis
 # driver needs to be install manually
 sudo apt install nvidia-driver-525 nvidia-dkms-525
@@ -48,4 +69,3 @@ sudo apt install nvidia-cuda-toolkit
 # --------------------------------------------------
 # 昨天做到这里
 # https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=deb_local
-
