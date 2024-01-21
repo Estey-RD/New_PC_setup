d33c1e144405b2902fbe2ed28061ee7f1b0f8627
Estey <30854444+VoidWind-ST@users.noreply.github.com>
2024-01-20 19:31:41 -0500

    Update check_system_info.sh

--- check_system_info.sh
+++ check_system_info.sh
@@ -1,2 +1,16 @@
 # --------------------------------------------------
-lscpu >> sys_info.txt
+mkdir sys_info
+
+lsb_release -a >> sys_info/0.sys_overview.txt
+
+lscpu >> sys_info/1.cpu_info.txt
+
+lsblk >> sys_info/2.blk_info.txt
+
+df -h >> sys_info/3.os_blk_info.txt
+
+free  >> sys_info/4.memory_info.txt
+
+xrandr >> sys_info/5.display_info.txt
+
+nvidia-smi >> sys_info/6.graphic_info.txt
