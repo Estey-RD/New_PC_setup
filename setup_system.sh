9810e4741fb12e172775a15989de6636b1123c14
Estey <30854444+VoidWind-ST@users.noreply.github.com>
2024-01-25 17:11:41 -0500

    Update setup_system.sh

--- setup_system.sh
+++ setup_system.sh
@@ -14,15 +14,7 @@
 
 # --------------------------------------------------
 # update all package first
-sudo apt-get update
-
-# --------------------------------------------------
-# git module
-sudo apt-get install git-all
-
-# --------------------------------------------------
-# htop module
-sudo snap install htop
+sudo apt update && sudo apt full-upgrade
 
 # --------------------------------------------------
 # build-essential
@@ -35,11 +27,33 @@ sudo snap install htop
 sudo apt install build-essential
 
 # --------------------------------------------------
+# git module
+sudo apt-get install git-all
+
+# --------------------------------------------------
+# apache2 module
+sudo apt-get install apache2
+
+# --------------------------------------------------
+# openssh-server module
+sudo apt-get install openssh-server
+
+# --------------------------------------------------
+# Firewall setting
+sudo apt-get install ufw
+sudo ufw enable
+sudo ufw allow ssh 
+
+# --------------------------------------------------
+# htop module
+sudo snap install htop
+
+# --------------------------------------------------
 # neofetch module
 sudo apt install neofetch
 
 # --------------------------------------------------
-# powertop module
+# powertop module， power monitor software, $ powertop to use
 sudo apt install powertop
 
 # --------------------------------------------------
@@ -47,23 +61,35 @@ sudo apt install powertop
 sudo apt install python3
 
 # --------------------------------------------------
-# anaconda 
-# 	Prerequisites pack:
-sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
-# 	Replace <INSTALLER_VERSION> with the version of the installer file you want to download
-# All installers can be found at repo.anaconda.com/archive
-#curl -O https://repo.anaconda.com/archive/Anaconda3-<INSTALLER_VERSION>-Linux-x86_64.sh
+# timeshift (restore point software)
+sudo apt install timeshift
+# create a snapshot 
+sudo timeshift --create
 
+# --------------------------------------------------
+# anaconda 
+#  Prerequisites pack:
+sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+#  installer package
+wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
+bash Anaconda3-2023.09-0-Linux-x86_64.sh -b
+rm -rf Anaconda3-2023.09-0-Linux-x86_64.sh
+
+# --------------------------------------------------
+# Vscode
+wget -O vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+sudo dpkg -i vscode.deb
+rm vscode.deb
 
 # --------------------------------------------------
 # Nvidia utilis
 # driver needs to be install manually
-sudo apt install nvidia-driver-525 nvidia-dkms-525
+#sudo apt install nvidia-driver-525 nvidia-dkms-525
 # Use this to open NV gui
 #nvidia-settings
 # --------------------------------------------------
 # Nvidia Cuda driver
-sudo apt install nvidia-cuda-toolkit
+#sudo apt install nvidia-cuda-toolkit
 # Use this to check cuda version
 #nvcc --version
 # --------------------------------------------------
