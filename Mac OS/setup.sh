653c38de4e1063f90472d86ccc4f59dc9e4c236a
Estey <30854444+Estey-RD@users.noreply.github.com>
2024-03-06 23:06:57 -0500

    Update setup.sh

--- Mac OS/setup.sh
+++ Mac OS/setup.sh
@@ -1,1 +1,12 @@
 
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> /Users/siennaque/.bash_profile
+eval "$(/usr/local/bin/brew shellenv)"
+brew update
+# Install some software
+brew install htop
+brew install python
+brew install zsh
+# Install VSocde
+brew install --cask visual-studio-code
