e5e74fa76f39b8110c5591af080eff6173dd9e23
Estey <30854444+Estey-RD@users.noreply.github.com>
2024-03-08 22:19:17 -0500

    Update setup.sh

--- Mac OS/setup.sh
+++ Mac OS/setup.sh
@@ -1,4 +1,5 @@
-
+# use git cmd to install some software
+git
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> /Users/siennaque/.bash_profile
@@ -11,6 +12,8 @@ brew install python
 brew install zsh
 # Rectangle, windows control plugin software
 brew install --cask rectangle
-# Install VSocde and iterm2
+# Install VSocde， iterm2 and Chrome
 brew install --cask visual-studio-code
 brew install --cask iterm2
+brew install --cask google-chrome
+
