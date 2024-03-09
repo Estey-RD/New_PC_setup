9dc7a66e44d27448376d8344bb404b75b3660e2c
Estey <30854444+Estey-RD@users.noreply.github.com>
2024-03-08 22:41:48 -0500

    Update setup.sh

--- Mac OS/setup.sh
+++ Mac OS/setup.sh
@@ -2,7 +2,7 @@
 git
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> /Users/siennaque/.bash_profile
+(echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> /Users/estey/.bash_profile    #change this user name
 eval "$(/usr/local/bin/brew shellenv)"
 brew update
 # Install some software
