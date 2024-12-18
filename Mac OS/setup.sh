c3771dfcd1b108e9b9df97062bcf39908de541cb
Estey <30854444+Estey-RD@users.noreply.github.com>
2024-12-18 12:32:41 -0500

    Update setup.sh

--- Mac OS/setup.sh
+++ Mac OS/setup.sh
@@ -43,9 +43,8 @@ brew install --cask paintbrush
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-#ZSH_THEME="powerlevel10k/powerlevel10k"
-#source ~/.zshrc
+brew install powerlevel10k
+echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 
 # Anaconda3
 curl -O https://repo.anaconda.com/archive/Anaconda3-2021.11-MacOSX-x86_64.sh
