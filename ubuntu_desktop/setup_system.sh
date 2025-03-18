8472f10327724483b9db3a09bf289cff61f90f22
Estey <30854444+Estey-RD@users.noreply.github.com>
2025-03-17 22:28:44 -0400

    Update setup_system.sh

--- ubuntu_desktop/setup_system.sh
+++ ubuntu_desktop/setup_system.sh
@@ -51,7 +51,7 @@ conda config --set auto_activate_base false
 # --------------------------------------------------
 # Vscode
 wget -O vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-sudo dpkg -i vscode.deb
+yes | sudo dpkg -i vscode.deb
 rm vscode.deb
 # --------------------------------------------------
 # Sublimetext
